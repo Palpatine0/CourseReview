24518279182407fac701e50108120e674595b9f7
sheevpalpatine <emperorsheevpalpatine0@gmail.com>
2024-04-17 15:56:45 +0800

    Update: reviews.php data completion

--- reviews.php
+++ reviews.php
@@ -13,33 +13,45 @@
         <main>
             <div class="container d-flex flex-column align-items-center">
                 <?php
-                $reviews = [
+                /*$reviews = [
                     [
-                        'name' => 'Zhang San',
+                        'name' => 'Palpatine',
                         'course' => 'Python Programming Introduction',
                         'content' => 'This is a very practical course that introduces the basics of programming in a simple and understandable way. The teacher explains clearly, the supporting materials are rich, and it is highly recommended for programming novices.'
                     ],
                     [
-                        'name' => 'Li Si',
+                        'name' => 'Don',
                         'course' => 'Data Analysis in Action',
                         'content' => 'I mastered the core technology of data analysis through this course. The course content covers a wide range, and the case practice has benefited me a lot and is worth learning.'
                     ],
                     [
-                        'name' => 'Wang Wu',
+                        'name' => 'Siri',
                         'course' => 'Machine Learning Algorithms',
                         'content' => 'This course explains the basic principles and common algorithms of machine learning. The teacher explains in a simple and understandable way, which gives me a deeper understanding of machine learning.'
                     ]
-                ]
+                ]*/
+                $sql = 'SELECT * FROM review';
+                $result = mysqli_query($conn, $sql);
+                $reviews = mysqli_fetch_all($result, MYSQLI_ASSOC);
                 ?>
                 <?php if (empty($reviews)): ?>
                     <p class="lead mt3">There are currently no course reviews!</p>
                 <?php endif; ?>
                 <h2>Course Reviews</h2>
 
-                <?php foreach($reviews as $review):?>
-                    <div class="card my-3" style="width: 18rem;">
-                        <div class="card-body">
+                <?php foreach ($reviews as $review): ?>
+                    <div class="card my-3" style="width: 28rem;">
+                        <div class="card-title text-center mt-2 ">
+                            <b><?php echo $review['course']; ?></b>
+                        </div>
+                        <div class="card-body text-center">
                             <?php echo $review['content']; ?>
+                            <div class="text-secondary mt-2">
+                                From <?php echo $review['name']; ?>
+                            </div>
+                            <div class="text-secondary mt-2">
+                                <?php echo $review['date'];?>
+                            </div>
                         </div>
                     </div>
                 <?php endforeach; ?>
